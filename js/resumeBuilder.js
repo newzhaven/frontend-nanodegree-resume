6e4e4ee10044fc61223d921c0f02b504c4fc2b1c
Tanat Iempreedee <tanat.newz@gmail.com>
2016-05-16 23:35:24 +0700

    Name and role added

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,10 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+ var formattedName, formattedRole;
+
+ formattedName = HTMLheaderName.replace("%data%", "Tanat Iempreedee");
+ formattedRole = HTMLheaderRole.replace("%data%", "IT Business Analyst");
+
+ $("#header").append(formattedName).append(formattedRole);
+ //$("#header").prepend(formattedRole).prepend(formattedName);
